8e6e13eef487cd2de0b90c8dbf05eedb86c11753
Sxvz <sami.backroos@gmail.com>
2016-12-22 17:52:49 +0200

    Created CommentController + refactored EventController/Service

--- src/main/java/calendar/controller/EventController.java
+++ src/main/java/calendar/controller/EventController.java
@@ -2,34 +2,27 @@ package calendar.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import calendar.domain.Event;
-import calendar.repository.AccountRepository;
-import calendar.repository.EventRepository;
 import calendar.service.EventService;
 
 @Controller
-
+@RequestMapping("/events")
 public class EventController {
 
     @Autowired
     private EventService eventService;
-    @Autowired
-    private EventRepository eRepo;
-    @Autowired
-    private AccountRepository accountRepo;
 
-    @RequestMapping(value = "/newevent", method = RequestMethod.GET)
-    public String newEvent(Model model) {
+    @RequestMapping(value = "/create")
+    public String newEvent() {
         return "newevent";
     }
 
-    @RequestMapping(value = "/events", method = RequestMethod.POST)
+    @RequestMapping(method = RequestMethod.POST)
     public String add(Event event) {
         eventService.save(event);
         return "redirect:/calendar";
     }
-    
+
 }
