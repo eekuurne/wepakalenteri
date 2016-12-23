a650493b0751a3e819c087d90f52f357c09cd31f
Sxvz <sami.backroos@gmail.com>
2016-12-23 13:04:45 +0200

    Added remove functionality to EventController

--- src/main/java/calendar/controller/EventController.java
+++ src/main/java/calendar/controller/EventController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import calendar.domain.Event;
 import calendar.repository.EventRepository;
+import calendar.service.AuthenticationService;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 
@@ -15,6 +16,8 @@ public class EventController {
 
     @Autowired
     private EventRepository eventRepo;
+    @Autowired
+    private AuthenticationService authService;
 
     @RequestMapping(value = "/create")
     public String newEvent() {
@@ -23,23 +26,41 @@ public class EventController {
 
     @RequestMapping(method = RequestMethod.POST)
     public String add(Event event) {
+        event.setOwner(authService.getUserLoggedIn());
         eventRepo.save(event);
-        
+
         return "redirect:/";
     }
-    
+
     @RequestMapping(value = "/{id}/edit", method = RequestMethod.GET)
     public String edit(@PathVariable Long id, Model model) {
         Event e = eventRepo.getOne(id);
+
+        if (!authService.getUserLoggedIn().equals(e.getOwner())) {
+            return "redirect:/";
+        }
+
         model.addAttribute("event", e);
-        
+
         return "edit_event";
     }
-    
+
     @RequestMapping(value = "/{id}/edit", method = RequestMethod.POST)
     public String handleEdit(Event e) {
-        eventRepo.save(e);
-        
+        if (authService.getUserLoggedIn().equals(e.getOwner())) {
+            eventRepo.save(e);
+        }
+
+        return "redirect:/";
+    }
+
+    @RequestMapping(value = "/{id}/remove", method = RequestMethod.GET)
+    public String remove(@PathVariable Long id) {
+        Event e = eventRepo.getOne(id);
+        if (authService.getUserLoggedIn().equals(e.getOwner())) {
+            eventRepo.delete(e);
+        }
+
         return "redirect:/";
     }
 
