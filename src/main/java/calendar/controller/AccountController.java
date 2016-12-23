fa6e40faea2044901d4f0baebfe32f276ccc9903
Sxvz <sami.backroos@gmail.com>
2016-12-23 16:04:56 +0200

    Refactoring

--- src/main/java/calendar/controller/AccountController.java
+++ src/main/java/calendar/controller/AccountController.java
@@ -2,13 +2,9 @@
 package calendar.controller;
 
 import calendar.service.AuthenticationService;
-import java.util.ArrayList;
-import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * A controller for handling accounts.
@@ -20,7 +16,7 @@ public class AccountController {
     private AuthenticationService authService;
     
     @RequestMapping("/login")
-    public String viewLogin(@RequestParam(required = false) String error) {//, Model model) {
+    public String viewLogin() {//@RequestParam(required = false) String error, Model model) {
         if (authService.getUserLoggedIn() != null) {
             return "redirect:/logout";
         }
