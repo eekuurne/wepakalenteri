0c919dae476f873e310c89252eb1cf03816e3ba6
Sxvz <sami.backroos@gmail.com>
2016-12-26 11:44:39 +0200

    Made CaledarController handle errorneous dates better

--- src/main/java/calendar/controller/CalendarController.java
+++ src/main/java/calendar/controller/CalendarController.java
@@ -13,21 +13,24 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class CalendarController {
-    
+
     @Autowired
     private DayService dayService;
-    
+
     @RequestMapping(value = "/calendar", method = RequestMethod.GET)
     public String list(Model model,
             @RequestParam(required = false) @DateTimeFormat(pattern = "dd.MM.yyyy") Date startDate,
             @RequestParam(required = false) @DateTimeFormat(pattern = "dd.MM.yyyy") Date endDate) {
+        
         if (startDate == null) {
             startDate = new Date(System.currentTimeMillis());
         }
-        if (endDate == null) {
-            endDate = new Date(startDate.getTime() + InitializationService.dayInMillis * 27);
+        if (endDate == null || endDate.before(startDate)) {
+            endDate = new Date(startDate.getTime() + InitializationService.dayInMillis * 22);
         }
+
         model.addAttribute("weeks", dayService.generateAndPopulateDays(startDate, endDate));
+
         return "calendar";
     }
 }
