6fb4a0790fcd5c89c7402dc2b355f34e2a533cba
Eero Kuurne <eero.kuurne@solinor.com>
2016-12-28 00:21:20 +0200

    Datepicker prefilled

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -1,3 +1,25 @@
 $(document).ready(function() {
   console.log("Javascript works!");
+
+  initDatepicker();
 });
+
+function initDatepicker() {
+  document.getElementById("startDate").value = getDateDaysFromNow(0); 
+  document.getElementById("endDate").value = getDateDaysFromNow(120); 
+}
+
+function getDateDaysFromNow(days) {
+  var today = new Date(new Date().getTime() + days * (24 * 60 * 60 * 1000));
+  var dd = today.getDate();
+  var mm = today.getMonth() + 1; //January is 0!
+  var yyyy = today.getFullYear();
+
+  if(dd<10) {
+      dd = '0' + dd;
+  } 
+  if(mm<10) {
+      mm = '0' + mm;
+  } 
+  return yyyy + '-' + mm + '-' + dd;
+}
