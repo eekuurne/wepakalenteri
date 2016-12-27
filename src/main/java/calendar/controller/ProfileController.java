790ca5cba56a4a97c78446b26707d0d9b91545b4
Sxvz <sami.backroos@gmail.com>
2016-12-27 17:38:09 +0200

    Fixed login issue + Added pending participations to ProfileController

--- src/main/java/calendar/controller/ProfileController.java
+++ src/main/java/calendar/controller/ProfileController.java
@@ -1,15 +1,13 @@
 package calendar.controller;
 
-import calendar.repository.AccountRepository;
+import calendar.repository.ParticipationRepository;
+import calendar.service.AuthenticationService;
 import calendar.service.FriendService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/profile")
@@ -17,13 +15,18 @@ public class ProfileController {
     
     @Autowired
     private FriendService friendService;
+    @Autowired
+    private AuthenticationService authService;
+    @Autowired
+    private ParticipationRepository partRepo;
     
     @RequestMapping(method = RequestMethod.GET)
     public String view(Model model) {
-        model.addAttribute("username", SecurityContextHolder.getContext().getAuthentication().getName());
+        model.addAttribute("username", authService.getUserLoggedIn().getUsername());
         model.addAttribute("friends", friendService.findFriends());
         model.addAttribute("requesters", friendService.getFriendRequesters());
         model.addAttribute("pending", friendService.getPendingRequests());
+        model.addAttribute("pendingParticipations", partRepo.findByAccountAndAccepted(authService.getUserLoggedIn(), false));
         
         return "profile";
     }
